385335b8c745a8fd3e4848d9805b0b3ed21bf1a5
洪祺瑜 <1069619201@qq.com>
2017-12-23 23:53:56 +0800

    密保问题

---
+++ app/src/main/java/com/example/dressassistant/Requestion.java
@@ -0,0 +1,16 @@
+package com.example.dressassistant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by 洪祺瑜 on 2017-12-23.
+ */
+
+public class Requestion extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.requestion);
+    }
+}
