6224be7656a46f1fc4de31e0b08fce2aa79eb390
lyh <1062675140@qq.com>
2017-12-25 10:59:45 +0800

    注册已经不会用户名昵称密码为空也能跳转，注册成功才能跳转到密保问题页面

--- app/src/main/java/com/example/dressassistant/Register.java
+++ app/src/main/java/com/example/dressassistant/Register.java
@@ -21,7 +21,7 @@ import java.io.IOException;
  */
 
 public class Register extends AppCompatActivity {
-    private static final String DB_NAME="dbuser.db";
+    private static final String DB_NAME="ds.db";
     private SQLiteDatabase db;
 
 //打开数据库
@@ -39,7 +39,7 @@ public class Register extends AppCompatActivity {
 
         try
         {
-            db.execSQL("CREATE TABLE IF NOT EXISTS tuserinfo(_id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR, userpwd VARCHAR, usertype INTEGER)");
+            db.execSQL("CREATE TABLE IF NOT EXISTS tuserinfo(_id INTEGER PRIMARY KEY, pers_UsID VARCHAR, pers_UsNa VARCHAR, pers_Password VARCHAR, pers_Phone VARCHAR, pers_HePi VARCHAR, pers_Birthday VARCHAR, pers_FILID VARCHAR, pers_FLID VARCHAR, pers_FaID VARCHAR, pers_CUID VARCHAR, pers_PlID VARCHAR, pers_FeID VARCHAR, pers_GDUID VARCHAR, pers_MyMe VARCHAR, pers_CPLID VARCHAR, pers_FCID VARCHAR, pers_Q1 VARCHAR, pers_Q2 VARCHAR, pers_Q3 VARCHAR, pers_A1 VARCHAR, pers_A2 VARCHAR, pers_A3 VARCHAR)");
         }
         catch(SQLException se)
         {
@@ -47,46 +47,7 @@ public class Register extends AppCompatActivity {
             Log.d("tag",String.format(msg,se.getClass(), se.getMessage()));
         }
     }
-//检查管理员是否存在，没用到
-    private boolean isExistAdmin()
-    {
-        Cursor cursor = db.rawQuery("select * from tuserinfo where usertype=1", null);
-        if(cursor.getCount()>0)
-        {
-            cursor.close();
-            return true;
-        }
-        else
-        {
-            cursor.close();
-            return false;
-        }
-    }
-    //数据库插入管理员，没用到
-    private void insertAdminInfo(){
-        String strUserName = "czl";
-        String strUserPwd="czl";
-        int iUserType=1;
-        TextView tv1 = (TextView) findViewById(R.id.tv1);
-        if(isExistAdmin() == false){
-            ContentValues cvUserInfo = new ContentValues();
-            cvUserInfo.put("username",strUserName);
-            cvUserInfo.put("userpwd", strUserPwd);
-            cvUserInfo.put("usertype",iUserType);
-            if(db != null)
-            {
-                db.insert("tuserinfo", null, cvUserInfo);
-                Log.d("msg","插入结束");
-                Toast.makeText(Register.this,"注册成功!", Toast.LENGTH_LONG).show();
-                tv1.setText("用户名：" + strUserName + "\n" + "密码：" + strUserPwd + "\n" + "类别：" + iUserType);
 
-            }
-        }
-        else
-        {
-            tv1.setText("已存在系统管理员\n 用户名：" + strUserName + "\n" + "密码：" + strUserPwd + "\n");
-        }
-    }
 
     //判断xml上的输入是否为空
     private boolean isStrEmpty(String strInput)
@@ -116,7 +77,7 @@ public class Register extends AppCompatActivity {
     //判断数据库中是否存在该用户名
     private boolean isExistUserName(String strUserName)
     {
-        Cursor cursor = db.rawQuery("select * from tuserinfo where username='" + strUserName +"'", null);
+        Cursor cursor = db.rawQuery("select * from PersInfo where pers_UsID='" + strUserName +"'", null);
         if(cursor.getCount()>0){
             cursor.close();
             return true;
@@ -128,25 +89,42 @@ public class Register extends AppCompatActivity {
     }
 
     //数据库插入用户
-    private void insertUserInfo(String strUserName, String strUserPwd)
+    private void insertUserInfo(String strUserName, String strUserNick ,String strUserPwd)
     {
         int iUserType = 0;
-        if(isExistUserName(strUserName) == false){
-            ContentValues cvRUserInfo = new ContentValues();
-            cvRUserInfo.put("username",strUserName);
-            cvRUserInfo.put("userpwd", strUserPwd);
-            cvRUserInfo.put("usertype", iUserType);
-            if(db != null)
-            {
-                db.insert("tuserinfo", null, cvRUserInfo);
-                Toast.makeText(Register.this,"注册成功！", Toast.LENGTH_SHORT).show();
+        if(isStrEmpty(strUserName) == false){
+            if(isStrEmpty(strUserNick) == false){
+                if(isStrEmpty(strUserPwd) == false){
+                    if(isExistUserName(strUserName) == false){
+                        ContentValues cvRUserInfo = new ContentValues();
+                        cvRUserInfo.put("pers_UsID",strUserName);//用户名
+                        cvRUserInfo.put("pers_UsNa", strUserNick);//昵称
+                        cvRUserInfo.put("pers_Password", strUserPwd);
+                        if(db != null)
+                        {
+                            db.insert("PersInfo", null, cvRUserInfo);
+                            Toast.makeText(Register.this,"注册成功！", Toast.LENGTH_SHORT).show();
+                            Intent intent=new Intent(Register.this,Requestion.class);
+                            startActivity(intent);
+                        }
+                    }
+                    else{
+                        Toast.makeText(Register.this, "您要注册的用户名已经存在！", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(Register.this,"密码不能为空！",Toast.LENGTH_SHORT).show();
+                }
+            }
+            else{
+                Toast.makeText(Register.this,"昵称不能为空！",Toast.LENGTH_SHORT).show();
             }
         }
         else{
-            Toast.makeText(Register.this, "您要注册的用户名已经存在！", Toast.LENGTH_SHORT).show();
+            Toast.makeText(Register.this,"用户名不能为空！",Toast.LENGTH_SHORT).show();
         }
-    }
 
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -168,10 +146,10 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 EditText EditText1 = (EditText) findViewById(R.id.editText1);   //读入xml用户名
+                EditText EditText = (EditText) findViewById(R.id.editText);     //读入xml昵称
                 EditText EditText2 = (EditText) findViewById(R.id.editText2);   //读入xml密码
-                insertUserInfo(EditText1.getText().toString(),EditText2.getText().toString());
-                Intent intent=new Intent(Register.this,Requestion.class);
-                startActivity(intent);
+                insertUserInfo(EditText1.getText().toString(),EditText.getText().toString(),EditText2.getText().toString());
+
             }
         });
     }
