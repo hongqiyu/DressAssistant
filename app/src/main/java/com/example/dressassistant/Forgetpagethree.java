03ca89e4d555513135a4b64d060fc17443596193
lyh <1062675140@qq.com>
2018-01-02 16:50:38 +0800

    Forgetpagethree成功

--- app/src/main/java/com/example/dressassistant/Forgetpagethree.java
+++ app/src/main/java/com/example/dressassistant/Forgetpagethree.java
@@ -2,9 +2,11 @@ package com.example.dressassistant;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,6 +32,31 @@ public class Forgetpagethree extends AppCompatActivity {
             return false;
         }
     }
+
+    //打开数据库
+    private void OpenCreateDB()
+    {
+        try
+        {
+            db = openOrCreateDatabase(DB_NAME, this.MODE_PRIVATE, null);
+        }
+        catch (Throwable e)
+        {
+            Log.e("tag","open error:" + e.getMessage());
+            db = null;
+        }
+
+        try
+        {
+            db.execSQL("CREATE TABLE IF NOT EXISTS PersInfo(_id INTEGER PRIMARY KEY AUTOINCREMENT, pers_UsID VARCHAR, pers_UsNa VARCHAR, pers_Password VARCHAR, pers_Phone VARCHAR, pers_HePi VARCHAR, pers_Birthday VARCHAR, pers_FILID VARCHAR, pers_FLID VARCHAR, pers_FaID VARCHAR, pers_CUID VARCHAR, pers_PlID VARCHAR, pers_FeID VARCHAR, pers_GDUID VARCHAR, pers_MyMe VARCHAR, pers_CPLID VARCHAR, pers_FCID VARCHAR, pers_Q1 VARCHAR, pers_Q2 VARCHAR, pers_Q3 VARCHAR, pers_A1 VARCHAR, pers_A2 VARCHAR, pers_A3 VARCHAR)");
+        }
+        catch(SQLException se)
+        {
+            String msg = "doInstall.error:[%s].%s";
+            Log.d("tag",String.format(msg,se.getClass(), se.getMessage()));
+        }
+    }
+
     //注册时，判断两次输入的密码是否相同
     private boolean isPwdSame(String strUserPwd, String strUserRePwd)
     {
@@ -45,9 +72,13 @@ public class Forgetpagethree extends AppCompatActivity {
         if(isStrEmpty(UserPwd) == false){
             if(isStrEmpty(reUserPwd) == false){
                 if(isPwdSame(UserPwd,reUserPwd)){
+                    //两种方法都可以
                     ContentValues cvUpdatePwd = new ContentValues();
-                    cvUpdatePwd.put("pers_PassWord",UserPwd);
-                    db.update("PersInfo",cvUpdatePwd,"pers_UsID=?",new String[]{UserName});
+                    cvUpdatePwd.put("pers_Password",UserPwd);
+                    db.update("PersInfo",cvUpdatePwd,"pers_UsID = ?",new String[] { UserName });
+                    //final String updateData = "update PersInfo set pers_Password = '"+UserPwd+"'where pers_UsID ='"+UserName+"'";
+                    //db.execSQL(updateData);
+                    Toast.makeText(Forgetpagethree.this,"修改成功，快去登录吧！",Toast.LENGTH_SHORT).show();
                     Intent intent=new Intent(Forgetpagethree.this,Login.class);
                     startActivity(intent);
                 }
@@ -56,7 +87,7 @@ public class Forgetpagethree extends AppCompatActivity {
                 }
             }
             else{
-                Toast.makeText(Forgetpagethree.this,"确认密码不能为空！",Toast.LENGTH_SHORT).show();
+                Toast.makeText(Forgetpagethree.this,"确认新密码不能为空！",Toast.LENGTH_SHORT).show();
             }
         }
         else{
@@ -67,8 +98,13 @@ public class Forgetpagethree extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forgetpagethree);
+
+
         Intent in = getIntent();
         final String UserName = in.getStringExtra("UserName");
+
+        //打开数据库
+        OpenCreateDB();
         //点击
         Button b=(Button) findViewById(R.id.button25);
         b.setOnClickListener(new View.OnClickListener() {
