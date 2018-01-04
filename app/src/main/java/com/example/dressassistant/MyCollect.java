2efa774215fc6b1b3e9d5bf063ef524f90849e86
洪祺瑜 <1069619201@qq.com>
2018-01-04 17:53:24 +0800

    我的收藏

--- app/src/main/java/com/example/dressassistant/MyCollect.java
+++ app/src/main/java/com/example/dressassistant/MyCollect.java
@@ -13,13 +13,5 @@ public class MyCollect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mycollect);
-        Button button2 = (Button)findViewById(R.id.button6);
-        button2.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MyCollect.this,MySpace.class);
-                startActivity(intent);
-            }
-        });
     }
 }
